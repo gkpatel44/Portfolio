14cbc6cfd05b54cc669111fdb18fc1c17e395daf
gaurang kathiriya <kathiriyagaurang44@gmail.com>
2021-12-17 13:51:47 +0530

    all components page created and modify

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projectss = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+
+  export const skills = ["html" , "css", "js" , "reactjs" , "nodejs", "mongodb","postgres","aws","git","github","firebase","netifly"]
+
+  export const testimonials = [{
+    quote: "Epic Todo App",
+    image:"https://images.financialexpress.com/2020/06/Google.jpg",
+    name: "Epic Todo App",
+    company: "Epic Todo App"
+  },
+  {
+    quote: "Epic Todo App",
+    image:"https://images.financialexpress.com/2020/06/Google.jpg",
+    name: "Epic Todo App",
+    company: "Epic Todo App"
+  },
+  {
+    quote: "Epic Todo App",
+    image:"https://images.financialexpress.com/2020/06/Google.jpg",
+    name: "Epic Todo App",
+    company: "Epic Todo App"
+  },
+  {
+    quote: "Epic Todo App",
+    image:"https://images.financialexpress.com/2020/06/Google.jpg",
+    name: "Epic Todo App",
+    company: "Epic Todo App"
+  }]
